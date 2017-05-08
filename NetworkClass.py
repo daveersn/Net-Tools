8fae53ceb449f5ff0eb9e0c24f645d6751904406
xDavi01 <xDavi01@gmail.com>
2017-05-08 18:52:16 +0200

    Added checkbutton menu

--- NetworkClass.py
+++ NetworkClass.py
@@ -10,7 +10,7 @@ class Net:
         self.port = 0
         self.last_port = 0
 
-    def single_scan(self, ip, port):
+    def scan(self, ip, port):
         self.ip = ip
         self.port = port
         if self.connect() == 0:
@@ -18,15 +18,6 @@ class Net:
         else:
             return 1
 
-    def range_scan(self, ip, port, last_port):
-        self.ip = ip
-        self.port = int(port)
-        self.last_port = int(last_port)
-        for self.port in range(self.port, self.last_port):
-            if self.connect() == 0:
-                return 0
-            else:
-                return 1
 
     def connect(self):
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
--- WindowClass.py
+++ WindowClass.py
@@ -3,7 +3,7 @@ from NetworkClass import *
 import socket
 
 net = Net()
-
+modes = [("Single scan", 1), ("Range scan", 2), ("Netmap", 3)]
 
 class Window:
     def __init__(self, root):
@@ -13,9 +13,13 @@ class Window:
         self.port = 0
         self.range = 0
         self.operating_mode = 1  # 0: Single port scan; 1: Range port scan; 2: Network mapper.
+        self.v = IntVar()
+        self.v.set(1)
         # Window frame
         self.frame_top = Frame(self.root)
         self.frame_top.pack(side=TOP)
+        self.frame_right = Frame(self.root)
+        self.frame_right.pack(side=RIGHT)
         self.ip_entry = Entry(self.frame_top, width=13)
         self.ip_entry.grid(row=1, column=0)
         self.scan_bt = Button(self.frame_top, text="Scan", command=self.start_scan)
@@ -36,6 +40,8 @@ class Window:
             self.range_entry.grid(row=1, column=2)
             self.range_label = Label(self.frame_top, text="Range")
             self.range_label.grid(row=0, column=2)
+        for self.txt in modes:
+            Radiobutton(self.frame_right, text=self.txt, padx=20, variable=self.v, command=self.get).pack(anchor=W)
 
     def start_scan(self):
         self.ip = self.ip_entry.get()
@@ -45,7 +51,7 @@ class Window:
         if self.operating_mode == 0:
             self.port = self.port_entry.get()
             print "[DEBUG] - Starting single port scan"
-            if net.single_scan(self.ip, self.port) == 0:
+            if net.scan(self.ip, self.port) == 0:
                 print "[DEBUG] - [V] Port {} on {} is opened".format(self.port, self.ip)
             else:
                 print "[DEBUG] - [X] Port {} on {} is closed".format(self.port, self.ip)
@@ -56,9 +62,11 @@ class Window:
             print self.port
             print self.range
             print "[DEBUG] - Starting range of ports scan"
-            for self.port in range(int(self.port), int(self.range)):
-                if net.single_scan(self.ip, self.port) == 0:
+            for self.port in range(int(self.port), int(self.range+1)):
+                if net.scan(self.ip, self.port) == 0:
                     print "[DEBUG] - [V] Port {} on {} is opened".format(self.port, self.ip)
                 else:
                     print "[DEBUG] - [X] Port {} on {} is closed".format(self.port, self.ip)
 
+    def get(self):
+        print self.v.get()
